8f88587961389dc1c9c76006260aa43e66a709a0
Suraj Gund <surajgund46@gmail.com>
2024-05-30 14:39:08 +0530

    React Done CSS Left

--- top-course-starter/src/App.js
+++ top-course-starter/src/App.js
@@ -1,26 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./Components/Navbar";
 import Filter from "./Components/Filter";
 import Cards from "./Components/Cards";
+//import { Toast } from "react-toastify/dist/components";
+//import toast from 'react-toastify';
+import toast from "react-toastify/dist/ReactToastify.css";
+
 
 import {apiUrl,filterData} from './data';
 const App = () => {
-
+  const [courses,setCourses] = useState('');
   useEffect(()=>{
     const fetchData = async () =>{
       try{
         const result = await fetch(apiUrl);
-        const data =await result.json();
-      }
-      catch(error){
+        const output=await result.json();
+        setCourses(output.data);
+
 
       }
+      catch(error){
+        toast.error("Something Wrong");
+      }
     }
-  });
+    fetchData();
+  },[]);
   return <div>
     <NavBar></NavBar>
     <Filter filterData = {filterData}></Filter>
-    <Cards></Cards>
+    <Cards courses={courses}></Cards>
   </div>;
 };
 
--- top-course-starter/src/Components/Cards.js
+++ top-course-starter/src/Components/Cards.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Card from './Card'
 
- const Cards = () => {
+ const Cards = ({courses}) => {
+  let allCourses = [];
+
+  const getCourses = ()=>{
+    Object.values(courses).forEach((courseCategory) =>{
+      courseCategory.forEach((course) =>{
+        allCourses.push(course);
+      })
+    })
+      return allCourses;
+    }
+  
   return (
-    <div></div>
+    <div>
+      {
+      getCourses().map((course)=>{
+        return <Card course = {course}></Card>
+      })
+      }
+    </div>
   )
-}
+  }
+
 
 export default Cards;
